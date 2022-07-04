ae40ec5808eaf4b3056e56a89f127211e610f963
RickyRaMx_c149 <ricardo.rmeza@live.com>
2022-07-04 15:41:07 -0500

    ✅ configure router

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import IniciarSesion from './layout/IniciarSesion'
 import Layout from './layout/Layout'
 import Inicio from './paginas/Inicio'
+import LoginForm from './paginas/LoginForm'
+import NuevoCliente from './paginas/NuevoCliente'
+import EditarCliente from './paginas/EditarCliente'
 
 function App() {
   
@@ -11,10 +14,13 @@ function App() {
     <BrowserRouter>
       <Routes>
           <Route path="/" element={<IniciarSesion />}>
-              <Route/>
+              <Route index element={<LoginForm />}/>
           </Route>
+          
           <Route path='/clientes' element={<Layout />}>
             <Route index element={<Inicio />} />
+            <Route path='nuevo' element={<NuevoCliente />} />
+            <Route path='editar/:id' element={<EditarCliente />} />
           </Route>
       </Routes>
     </BrowserRouter>
